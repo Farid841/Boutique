59a3b406b5bbbd103b77e6fb132365c3f67ba065
Farid841 <79144349+Farid841@users.noreply.github.com>
2021-06-14 22:42:50 +0200

    add paramettres route

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
@@ -11,6 +12,11 @@ class TestController
     {
         dd("ça fonctionne très bien");
     }
+
+    /**
+     * @Route( "/test/{age<\d+>?0}", name="test", methods={"GEST","POST"}, host="localhost", schemes={"http","https"})
+     */
+
     public function test(Request $request, $age)
     {
         dump($request);
